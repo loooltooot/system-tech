418ee08aecd19137b4c950524cbd7de21220cb95
Emir Zakrewski <frenkchambers@gmail.com>
2023-07-25 16:27:45 +0300

    ci: framer motion

--- src/components/welcome-section/welcomeSection.tsx
+++ src/components/welcome-section/welcomeSection.tsx
@@ -3,18 +3,19 @@
 import Image from "next/image";
 import Hero from "./__hero/hero";
 import styles from './welcomeSection.module.scss';
-import { useEffect } from "react";
-import { gsap } from "gsap";
+import { motion } from 'framer-motion'
 
 export default function WelcomeSection() {
-    useEffect(() => {
-        gsap.from('.quinta-logo', { y: 50, opacity: 0, duration: 1 })
-    }, [])
-
     return (
         <section className={styles.welcome}>
             <Hero />
-            <Image className="quinta-logo" src="/img/quinta.png" alt="Quinta Studio" width={274} height={88} />
+            <motion.div
+                initial={{ y: 50, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: .6 }}
+            >
+                <Image src="/img/quinta.png" alt="Quinta Studio" width={274} height={88} />
+            </motion.div>
         </section>
     )
 }
