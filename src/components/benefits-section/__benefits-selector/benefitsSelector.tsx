c65eae170293e631d456891142f3407421064cd9
Emir Zakrewski <frenkchambers@gmail.com>
2023-07-30 18:03:35 +0300

    feat(benefits)

--- src/components/benefits-section/__benefits-selector/benefitsSelector.tsx
+++ src/components/benefits-section/__benefits-selector/benefitsSelector.tsx
@@ -1,17 +1,59 @@
-import { useState } from "react"
+'use client'
 
-interface iBenefit {
+import { useState } from "react"
+import Image from "next/image"
+import styles from './benefitsSelector.module.scss'
+
+export interface iBenefit {
     title: string,
     description: string,
     img: string,
 }
 
 export function BenefitsSelector({ benefits }: { benefits: iBenefit[] }) {
-    const [selected, setSelected] = useState()
+    const [selected, setSelected] = useState(0)
 
     return (
-        <div>
-
+        <div className={styles.selector}>
+            <ul>
+                {benefits.map((benefit, index) => (
+                    <li key={index} onClick={() => { setSelected(index) }}>
+                        <h3
+                            className={selected === index ? styles.active : undefined}
+                        >
+                            <BubbleDecal className={selected === index ? styles.visible : undefined} />
+                            {benefit.title}
+                        </h3>
+                        <p>{benefit.description}</p>
+                    </li>
+                ))}
+            </ul>
+            <div className="img-wrapper">
+                <Image
+                    src={`/img/benefits/${benefits[selected].img}`}
+                    alt={benefits[selected].title}
+                    width={658}
+                    height={545}
+                    loading="lazy"
+                />
+            </div>
         </div>
+    )
+}
+
+function BubbleDecal({ className }: { className?: string }) {
+    return (
+        <svg className={className} xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 35 35" fill="none">
+            <g filter="url(#filter0_f_207_31)">
+                <circle cx="17.5" cy="17.5" r="9.5" fill="#37DE6A" />
+            </g>
+            <defs>
+                <filter id="filter0_f_207_31" x="0" y="0" width="35" height="35" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                    <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                    <feGaussianBlur stdDeviation="4" result="effect1_foregroundBlur_207_31" />
+                </filter>
+            </defs>
+        </svg>
     )
 }
--- src/components/benefits-section/benefitsSection.tsx
+++ src/components/benefits-section/benefitsSection.tsx
@@ -1,15 +1,33 @@
+import { BenefitsSelector, iBenefit } from './__benefits-selector/benefitsSelector'
 import styles from './benefitsSection.module.scss'
+
+const benefits: iBenefit[] = [
+    {
+        title: 'простота использования',
+        description: '3D – декали проще в работе, чем интерьеры, выполненные по традиционным технологиям. Вам нет необходимости заниматься тщательной подгонкой и окраской',
+        img: '1.png',
+    },
+    {
+        title: 'рельеф и фактура',
+        description: 'На 3D-декалях мы сделали рельефными такие детали, как утопленные циферблаты приборов и их выступающие рамки, блоки и тумблеры, кнопки и сигнальные лампы, рычаги и рукоятки, клепки и головки винтов.',
+        img: '2.png',
+    },
+    {
+        title: 'долговечность',
+        description: 'Материал изделия – полимеризованный винил. Поэтому 3D-декали имеют достаточный запас прочности и эластичности, так что при умеренной аккуратности в работе можно не бояться повредить их.',
+        img: '3.png',
+    },
+]
 
 export function BenefitsSection() {
     return (
-        <section className={styles.benefits}>
-            <header>
+        <section className={styles.benefits} id='benefits'>
+            <header className={styles.heroHeader}>
                 <h2 style={{ opacity: "0", position: "absolute", zIndex: "-1" }}>3 причины выбрать нас</h2>
-                <div className={styles.heroHeader}>
-                    <span className={styles.bigNum}>3</span>
-                    <span>причины выбрать нас</span>
-                </div>
+                <span className={styles.bigNum}>{benefits.length}</span>
+                <span>причины выбрать нас</span>
             </header>
+            <BenefitsSelector benefits={benefits} />
         </section>
     )
 }
