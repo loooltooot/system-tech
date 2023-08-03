6d4e8bc0f8a5ff09186c023e4a846951b4741930
Emir Zakrewski <frenkchambers@gmail.com>
2023-08-03 14:14:27 +0300

    feat: slider

--- src/components/gallery-section/__slider/slider.tsx
+++ src/components/gallery-section/__slider/slider.tsx
@@ -1,16 +1,21 @@
 'use client'
 
 import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation } from 'swiper/modules'
+import 'swiper/scss'
+import 'swiper/scss/navigation'
 import Image from 'next/image'
 import styles from './slider.module.scss'
 
 export function Slider({ images }: { images: string[] }) {
     return (
         <Swiper
+            modules={[Navigation]}
             slidesPerView={2}
             className={styles.slider}
             loop={true}
             initialSlide={2}
+            navigation
         >
             {images.map((img, index) => (
                 <SwiperSlide
