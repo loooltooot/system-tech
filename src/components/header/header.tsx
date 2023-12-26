15e8e87d35729956cbe69fa0765693c52b7754bb
Emir Zakrewski <frenkchambers@gmail.com>
2023-12-26 17:08:58 +0300

    ⚗️: footer is done

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,22 +3,8 @@
 import Image from 'next/image'
 import Nav from '@/components/nav/nav'
 import styles from './header.module.scss'
-import { useEffect } from 'react';
 
 export default function Header() {
-    useEffect(() => {
-        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-            anchor.addEventListener('click', function (e) {
-                e.preventDefault();
-
-                // @ts-ignore
-                document.querySelector(this.getAttribute('href')).scrollIntoView({
-                    behavior: 'smooth'
-                });
-            });
-        });
-    }, [])
-
     return (
         <header className={styles.header}>
             <Image alt='Системотехника' src="/img/logo.svg" width={388} height={26.4} />
